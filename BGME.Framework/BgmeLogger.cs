76debe19287ad186b6c449463c90e14c301d62fe
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-22 15:36:38 -0500

    Add serilog.

---
+++ BGME.Framework/BgmeLogger.cs
@@ -0,0 +1,29 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Serilog.Formatting;
+using Reloaded.Mod.Interfaces;
+using System.Drawing;
+
+namespace BGME.Framework;
+
+internal class BgmeLogger : ILogEventSink
+{
+    private readonly ILogger log;
+    private readonly ITextFormatter formatter = new MessageTemplateTextFormatter("[BGME Framework] [{Level}] {Message:lj}", null);
+
+    public BgmeLogger(ILogger log)
+    {
+        this.log = log;
+    }
+
+    public void Emit(LogEvent logEvent)
+    {
+        var message = new StringWriter();
+        formatter.Format(logEvent, message);
+        var color = logEvent.Level == LogEventLevel.Error ? Color.Red
+            : logEvent.Level == LogEventLevel.Debug ? Color.Green : Color.White;
+
+        this.log.WriteLine(message.ToString(), color);
+    }
+}
