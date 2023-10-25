bf19db0933c641b0bf39324381231d6f7d4d3446
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-25 01:13:41 -0500

    P3P: Add global bgm support. Handle playing bgm file that doesn't exist.

--- BGME.Framework/P3P/SoundPatcher.cs
+++ BGME.Framework/P3P/SoundPatcher.cs
@@ -1,4 +1,5 @@
-﻿using Reloaded.Hooks.Definitions;
+﻿using BGME.Framework.Music;
+using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.Enums;
 using Reloaded.Hooks.Definitions.X64;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
@@ -12,7 +13,7 @@ namespace BGME.Framework.P3P;
 /// <summary>
 /// Patch BGM function to play any BGM audio file.
 /// </summary>
-internal unsafe class SoundPatcher
+internal unsafe class SoundPatcher : BaseSound
 {
     private const int MAX_STRING_SIZE = 16;
 
@@ -23,7 +24,11 @@ internal unsafe class SoundPatcher
 
     private readonly byte* bgmStringBuffer;
 
-    public SoundPatcher(IReloadedHooks hooks, IStartupScanner scanner)
+    public SoundPatcher(
+        IReloadedHooks hooks,
+        IStartupScanner scanner,
+        MusicService music)
+        : base(music)
     {
         this.bgmStringBuffer = (byte*)NativeMemory.AllocZeroed(MAX_STRING_SIZE, sizeof(byte));
         scanner.AddMainModuleScan("4E 8B 84 C3 28 4C 81 00", result =>
@@ -50,11 +55,17 @@ internal unsafe class SoundPatcher
 
     private byte* GetBgmStringImpl(int bgmId)
     {
-        var bgmString = string.Format("{0:00}.ADX\0", bgmId);
+        var bgmString = string.Format("{0:00}.ADX\0", this.GetGlobalBgmId(bgmId) ?? bgmId);
         if (bgmString.Length > MAX_STRING_SIZE)
         {
             bgmString = "01.ADX\0";
             Log.Error("BGM value too large. Value: {value}", bgmId);
+        }
+
+        if (bgmId < 1)
+        {
+            bgmString = "01.ADX\0";
+            Log.Error("Negative BGM value, previous file probably does not exist.", bgmId);
         }
 
         var bgmStringBytes = Encoding.ASCII.GetBytes(bgmString);
