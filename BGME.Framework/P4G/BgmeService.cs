baa400d300419a1496e0e3721565ad9c4dc1ba9e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-25 01:20:58 -0500

    Update bgme for P4G and P3P.

--- BGME.Framework/P4G/BgmeService.cs
+++ BGME.Framework/P4G/BgmeService.cs
@@ -2,18 +2,18 @@
 using Reloaded.Hooks.Definitions;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
 
-namespace BGME.Framework.P3P;
+namespace BGME.Framework.P4G;
 
 internal class BgmeService : IBgmeService
 {
     private readonly SoundPatcher soundPatcher;
     private readonly EncounterBgm encounterPatcher;
-    private readonly FloorBgm floorBgm;
+    private readonly FloorBgm floorPatcher;
 
     public BgmeService(IReloadedHooks hooks, IStartupScanner scanner, MusicService music)
     {
-        this.soundPatcher = new(hooks, scanner, music);
+        this.soundPatcher = new(hooks, music);
         this.encounterPatcher = new(hooks, scanner, music);
-        this.floorBgm = new(hooks, scanner, music);
+        this.floorPatcher = new(hooks, scanner, music);
     }
 }
