7e01393e0e32e4f162ab64226f702600baf7dd67
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-26 07:33:04 -0500

    P5R: Add initial bgme service.

---
+++ BGME.Framework/P5R/BgmeService.cs
@@ -0,0 +1,17 @@
+﻿using BGME.Framework.Music;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+
+namespace BGME.Framework.P5R;
+
+internal class BgmeService : IBgmeService
+{
+    private readonly Sound sound;
+    private readonly EncounterBgm encounterBgm;
+
+    public BgmeService(IReloadedHooks hooks, IStartupScanner scanner, MusicService music)
+    {
+        this.sound = new(hooks, scanner, music);
+        this.encounterBgm = new(hooks, scanner, this.sound, music);
+    }
+}
