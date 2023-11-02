1b14229cb379cb968e31665e032abc9d6bafdcad
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-02 03:39:40 -0500

    Add config files.

---
+++ BGME.Framework/Template/Configuration/Config.cs
@@ -0,0 +1,24 @@
+﻿namespace BGME.Framework.Template.Configuration;
+
+using System.ComponentModel;
+
+public class Config : Configurable<Config>
+{
+    [DisplayName("Hot Reload")]
+    [Description("Rebuild game files on music script changes.")]
+    [DefaultValue(false)]
+    public bool HotReload { get; set; }
+
+    [DisplayName("Log Level")]
+    [DefaultValue(LogLevel.Information)]
+    public LogLevel LogLevel { get; set; } = LogLevel.Information;
+}
+
+/// <summary>
+/// Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+/// Override elements in <see cref="ConfiguratorMixinBase"/> for finer control.
+/// </summary>
+public class ConfiguratorMixin : ConfiguratorMixinBase
+{
+    // 
+}
