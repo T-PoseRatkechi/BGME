88ebb1fd87d54d3c2245ed305f15db5609de4b9d
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-07 10:59:43 -0600

    P5R: Fix possible cause for crashes.

--- BGME.Framework/P5R/EncounterBgm.cs
+++ BGME.Framework/P5R/EncounterBgm.cs
@@ -42,8 +42,8 @@ internal unsafe class EncounterBgm : BaseEncounterBgm
                 $"{Utilities.PopCallerRegisters}",
                 "cmp eax, -1",
                 "jng original",
-                $"{hooks.Utilities.GetAbsoluteCallMnemonics(unchecked((nint) 0x140974A20), true)}",
-                $"{hooks.Utilities.GetAbsoluteJumpMnemonics(unchecked((nint) 0x1409786EB), true)}",
+                $"{hooks.Utilities.GetAbsoluteCallMnemonics(Utilities.BaseAddress + 0x974A20, true)}",
+                $"{hooks.Utilities.GetAbsoluteJumpMnemonics(Utilities.BaseAddress + 0x9786EB, true)}",
                 $"label original"
             };
 
