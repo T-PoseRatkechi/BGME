9e1c4007a9c7919cf4c9b5447115695ac6c01942
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-11 23:28:40 -0600

    Add API interface.

---
+++ BGME.Framework.Interfaces/IBgmeApi.cs
@@ -0,0 +1,16 @@
+﻿namespace BGME.Framework.Interfaces;
+
+public interface IBgmeApi
+{
+    /// <summary>
+    /// Add a folder to load music scripts from.
+    /// </summary>
+    /// <param name="folder">Folder path.</param>
+    void AddFolder(string folder);
+
+    /// <summary>
+    /// Remove a folder for loading music scripts.
+    /// </summary>
+    /// <param name="folder">Folder path.</param>
+    void RemoveFolder(string folder);
+}
