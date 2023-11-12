d62ac48ac70254f08b87eeccac1cd71451fd5cf6
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-11 22:19:33 -0600

    Adjust global bgm so sequential plays of an original song only randomize once.

--- BGME.Framework/Music/BaseSound.cs
+++ BGME.Framework/Music/BaseSound.cs
@@ -1,22 +1,37 @@
-﻿namespace BGME.Framework.Music;
+﻿using PersonaMusicScript.Library.Models;
+
+namespace BGME.Framework.Music;
 
 internal abstract class BaseSound
 {
     private readonly MusicService music;
+
+    private int prevOriginalBgmId;
+    private int newBgmId;
 
     public BaseSound(MusicService music)
     {
         this.music = music;
     }
 
-    protected int GetGlobalBgmId(int bgmId)
+    protected int GetGlobalBgmId(int originalBgmId)
     {
-        if (this.music.Global.TryGetValue(bgmId, out var newMusic))
+        if (this.music.Global.TryGetValue(originalBgmId, out var newMusic))
         {
-            Log.Debug($"Global BGM overwriting BGM ID: {bgmId}");
-            return Utilities.CalculateMusicId(newMusic);
+            Log.Debug($"Global BGM overwriting BGM ID: {originalBgmId}");
+            if (this.prevOriginalBgmId == originalBgmId && newMusic.Type == MusicType.RandomSong)
+            {
+                Log.Debug("Reusing previous random song.");
+                return this.newBgmId;
+            }
+
+            this.prevOriginalBgmId = originalBgmId;
+            this.newBgmId = Utilities.CalculateMusicId(newMusic);
+
+            return this.newBgmId;
         }
 
-        return bgmId;
+        this.prevOriginalBgmId = originalBgmId;
+        return originalBgmId;
     }
 }
