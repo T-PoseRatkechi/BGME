bb7ec481a63a3d5a21cbdac21575f41592439841
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-03 03:23:12 -0600

    Add API functions for adding/removing music entries.

--- BGME.Framework.Interfaces/IBgmeApi.cs
+++ BGME.Framework.Interfaces/IBgmeApi.cs
@@ -13,4 +13,16 @@ public interface IBgmeApi
     /// </summary>
     /// <param name="folder">Folder path.</param>
     void RemoveFolder(string folder);
+
+    /// <summary>
+    /// Add a music entry.
+    /// </summary>
+    /// <param name="callback">Callback that returns a new music entry.</param>
+    void AddEntry(Func<object> callback);
+
+    /// <summary>
+    /// Remove an existing music entry.
+    /// </summary>
+    /// <param name="callback">Previously added callback to remove.</param>
+    void RemoveEntry(Func<object> callback);
 }
