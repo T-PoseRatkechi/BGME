8654dc4a54364d66975ea6a8abd6f13a3c059d42
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-21 20:19:55 -0600

    Move CriAtomEx types to own files.

---
+++ BGME.Framework/CRI/Types/CRIATOM_FORMAT.cs
@@ -0,0 +1,13 @@
+﻿namespace BGME.Framework.CRI.Types;
+
+internal enum CRIATOM_FORMAT
+    : uint
+{
+    NONE,
+    ADX,
+    HCA = 3,
+    HCA_MX,
+    WAVE,
+    RAW_PCM,
+    AIFF,
+}
---
+++ BGME.Framework/CRI/Types/CriBool.cs
@@ -0,0 +1,7 @@
+﻿namespace BGME.Framework.CRI.Types;
+
+internal enum CriBool
+{
+    CRI_TRUE,
+    CRI_FALSE,
+}
