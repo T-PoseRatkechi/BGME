d5906b33991d40fa4ab9158957b5d3369d7056a7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-24 20:33:08 -0600

    P4G: Bind mod music. Update mod.

--- BGME.Framework/Mod.cs
+++ BGME.Framework/Mod.cs
@@ -11,12 +11,16 @@ using Reloaded.Hooks.ReloadedII.Interfaces;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
 using Reloaded.Mod.Interfaces.Internal;
+using System.Diagnostics;
 using System.Drawing;
+using System.Text.RegularExpressions;
 
 namespace BGME.Framework;
 
 public class Mod : ModBase, IExports
 {
+    private static readonly Regex numReg = new(@"\d+");
+
     private readonly IModLoader modLoader;
     private readonly IReloadedHooks hooks;
     private readonly ILogger logger;
@@ -30,6 +34,7 @@ public class Mod : ModBase, IExports
     private readonly Game game;
     private readonly MusicService? music;
     private readonly MusicScriptsManager musicScripts = new();
+    private readonly CriAtomEx criAtomEx;
 
     public Mod(ModContext context)
     {
@@ -72,15 +77,18 @@ public class Mod : ModBase, IExports
         switch (game)
         {
             case Game.P4G_PC:
-                this.bgme = new P4G.BgmeService(this.hooks, scanner!, this.music);
+                this.criAtomEx = new CriAtomEx(game);
+                this.criAtomEx.Initialize(scanner!, this.hooks);
+                this.bgme = new P4G.BgmeService(this.config, this.criAtomEx, this.music);
+                this.bgme.Initialize(scanner!, hooks);
                 break;
             case Game.P3P_PC:
                 this.bgme = new P3P.BgmeService(this.hooks, scanner!, this.music);
                 break;
             case Game.P5R_PC:
-                var criAtomEx = new CriAtomEx(game);
-                criAtomEx.Initialize(scanner!, this.hooks);
-                this.bgme = new P5R.BgmeService(this.p5rLib, criAtomEx, this.music);
+                this.criAtomEx = new CriAtomEx(game);
+                this.criAtomEx.Initialize(scanner!, this.hooks);
+                this.bgme = new P5R.BgmeService(this.p5rLib, this.criAtomEx, this.music);
                 this.bgme.Initialize(scanner!, hooks);
                 break;
             default:
@@ -130,6 +138,39 @@ public class Mod : ModBase, IExports
                 }
             }
         }
+        else if (this.game == Game.P4G_PC)
+        {
+            Log.Debug("Binding BGME P4G music.");
+
+            var p4gMusicDir = Path.Join(modDir, "bgme", "p4g");
+            if (Directory.Exists(p4gMusicDir))
+            {
+                foreach (var file in Directory.EnumerateFiles(p4gMusicDir, "*.hca"))
+                {
+                    var bindPath = Path.GetRelativePath(modDir, file);
+                    this.criFsApi.AddBind(file, bindPath, "BGME.Framework");
+                }
+            }
+
+            var p4gAwbDir = Path.Join(modDir, "FEmulator", "AWB", "snd00_bgm.awb");
+            if (Directory.Exists(p4gAwbDir))
+            {
+                foreach (var file in Directory.EnumerateFiles(p4gAwbDir, "*.hca"))
+                {
+                    var awbIndex = GetAwbIndex(file);
+                    if (awbIndex >= 678)
+                    {
+                        this.criFsApi.AddBind(file, $"BGME/P4G/{awbIndex}.hca", "BGME.Framework");
+                    }
+                }
+            }
+        }
+    }
+
+    private static int GetAwbIndex(string file)
+    {
+        var index = int.Parse(numReg.Match(Path.GetFileNameWithoutExtension(file)).Groups[0].Value);
+        return index;
     }
 
     private static Game GetGame(string appId)
@@ -158,6 +199,15 @@ public class Mod : ModBase, IExports
     }
 
     public Type[] GetTypes() => new[] { typeof(IBgmeApi) };
+
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
+    {
+        // Apply settings from configuration.
+        // ... your code here.
+        logger.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
+    }
+    #endregion
 
     #region For Exports, Serialization etc.
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
