aff1f0fc5ad649b14a79cb9b0313d7571e7826cf
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-01-24 20:30:06 -0600

    P4G: Reimplement BGM functionality.

---
+++ BGME.Framework/P4G/BgmPlayback.cs
@@ -0,0 +1,80 @@
+﻿using BGME.Framework.CRI;
+using BGME.Framework.Music;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.Definitions.X64;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using static Reloaded.Hooks.Definitions.X64.FunctionAttribute;
+
+namespace BGME.Framework.P4G;
+
+internal unsafe class BgmPlayback : BaseSound, IGameHook
+{
+    [Function(CallingConventions.Microsoft)]
+    private delegate void PlaySoundFunction(int soundCategory, int soundId, nint param3, nint param4);
+    private IFunction<PlaySoundFunction>? playSoundFunction;
+    private IHook<PlaySoundFunction>? playSoundHook;
+
+    [Function(new Register[] { Register.rbx, Register.rax }, Register.rax, true)]
+    private delegate void GetBgmAcbPtr(nint acbStrPtr, nint acbHndlPtr);
+    private IReverseWrapper<GetBgmAcbPtr>? bgmAcbWrapper;
+    private IAsmHook? bgmAcbHook;
+
+    private readonly CriAtomEx criAtomEx;
+    private PlayerConfig? bgmPlayer;
+
+    public BgmPlayback(CriAtomEx criAtomEx, MusicService music)
+        : base(music)
+    {
+        this.criAtomEx = criAtomEx;
+    }
+
+    public PlayerConfig BgmPlayer
+    {
+        get
+        {
+            this.bgmPlayer ??= this.criAtomEx.GetPlayerByAcbPath("/sound/adx2/bgm/snd00_bgm.acb");
+            return this.bgmPlayer!;
+        }
+    }
+
+    public void Initialize(IStartupScanner scanner, IReloadedHooks hooks)
+    {
+        scanner.Scan("Play Sound Function", "48 63 E9 89 D6", result =>
+        {
+            this.playSoundFunction = hooks.CreateFunction<PlaySoundFunction>(result - 30);
+            this.playSoundHook = this.playSoundFunction.Hook(this.PlaySoundImpl).Activate();
+        });
+    }
+
+    protected override void PlayBgm(int bgmId)
+    {
+        this.playSoundFunction?.GetWrapper()(0, bgmId, 0, 0);
+    }
+
+    /// <summary>
+    /// Play sound function.
+    /// </summary>
+    /// <param name="soundCategory">Sound category? 0 to 3 = BGM, 4 = SFX</param>
+    /// <param name="soundId">Sound ID to play? Equal to Cue ID for BGM, unknown value for others.</param>
+    /// <param name="param3">Unknown param.</param>
+    /// <param name="param4">Unknown param.</param>
+    /// <returns></returns>
+    private void PlaySoundImpl(int soundCategory, int soundId, nint param3, nint param4)
+    {
+        if (soundCategory != 0)
+        {
+            Log.Verbose($"Playing Sound ID: {soundId}");
+            this.playSoundHook!.OriginalFunction(soundCategory, soundId, param3, param4);
+            return;
+        }
+
+        var currentBgmId = this.GetGlobalBgmId(soundId);
+        if (currentBgmId == null)
+        {
+            return;
+        }
+
+        Log.Debug($"Playing BGM ID: {currentBgmId}");
+        this.playSoundHook!.OriginalFunction(soundCategory, (int)currentBgmId, param3, param4);
+    }
+}
--- BGME.Framework/P4G/BgmeService.cs
+++ BGME.Framework/P4G/BgmeService.cs
@@ -1,25 +1,104 @@
-﻿using BGME.Framework.Music;
+﻿using BGME.Framework.CRI;
+using BGME.Framework.CRI.Types;
+using BGME.Framework.Music;
+using BGME.Framework.Template.Configuration;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using static BGME.Framework.CRI.CriAtomExFunctions;
 
 namespace BGME.Framework.P4G;
 
-internal class BgmeService : IBgmeService
+internal class BgmeService : IBgmeService, IGameHook
 {
-    private readonly Sound sound;
-    private readonly EncounterBgm encounterPatcher;
-    private readonly FloorBgm floorPatcher;
-    private readonly EventBgm eventBgm;
+    private readonly Config config;
+    private readonly CriAtomEx criAtomEx;
+    private readonly MusicService music;
 
-    public BgmeService(IReloadedHooks hooks, IStartupScanner scanner, MusicService music)
+    private BgmPlayback bgm;
+    private EncounterBgm encounterPatcher;
+    private FloorBgm floorPatcher;
+    private EventBgm eventBgm;
+
+    private IHook<criAtomExPlayer_SetCueId>? setCueIdHook;
+    private PlayerConfig? bgmPlayer;
+
+    public BgmeService(Config config, CriAtomEx criAtomEx, MusicService music)
     {
-        this.sound = new(hooks, scanner, music);
-        this.encounterPatcher = new(hooks, scanner, music);
-        this.floorPatcher = new(hooks, scanner, music);
-        this.eventBgm = new(hooks, scanner, this.sound, music);
+        this.config = config;
+        this.criAtomEx = criAtomEx;
+        this.music = music;
+
+        this.bgm = new(this.criAtomEx, this.music);
+        this.encounterPatcher = new(music);
+        this.floorPatcher = new(music);
+        this.eventBgm = new(this.bgm, music);
+
+        this.criAtomEx.SetPlayerConfigById(0, new()
+        {
+            maxPath = 512,
+            maxPathStrings = 2,
+        });
+
+        criAtomEx.PropertyChanged += (sender, args) =>
+        {
+            if (args.PropertyName == nameof(criAtomEx.SetCueId))
+            {
+                this.setCueIdHook = criAtomEx.SetCueId!.Hook(this.CriAtomExPlayer_SetCueId).Activate();
+            }
+        };
     }
 
     public void Initialize(IStartupScanner scanner, IReloadedHooks hooks)
     {
+        this.bgm.Initialize(scanner, hooks);
+        this.encounterPatcher.Initialize(scanner, hooks);
+        this.floorPatcher.Initialize(scanner, hooks);
+        this.eventBgm.Initialize(scanner, hooks);
     }
+
+    private unsafe void CriAtomExPlayer_SetCueId(nint player, nint acbHn, int cueId)
+    {
+        this.bgmPlayer ??= this.criAtomEx.GetPlayerByAcbPath("/sound/adx2/bgm/snd00_bgm.acb");
+
+        if (player == this.bgmPlayer?.PlayerHn && IsBgmeMusic(cueId))
+        {
+            Log.Debug($"{nameof(CriAtomExPlayer_SetCueId)}|BGME: {player:X} || {acbHn:X} || {cueId}");
+
+            var bgmFile = this.GetBgmFile(cueId);
+            var format = Path.GetExtension(bgmFile) == ".hca" ? CRIATOM_FORMAT.HCA : CRIATOM_FORMAT.ADX;
+            var ptr = StringsCache.GetStringPtr(bgmFile);
+
+            this.criAtomEx.Player_SetFile(player, IntPtr.Zero, (byte*)ptr);
+            this.criAtomEx.Player_SetFormat(player, format);
+            this.criAtomEx.Player_SetNumChannels(player, 2);
+            this.criAtomEx.Player_SetSamplingRate(player, 48000);
+
+            // BGM isn't stopped by SFX so maybe correct ID?
+            // Does not respect music volume level though.
+            // Does not respect ANY KIND OF VOLUME setting...
+            this.criAtomEx.Player_SetCategoryById(player, 13);
+        }
+        else
+        {
+            this.setCueIdHook!.OriginalFunction(player, acbHn, cueId);
+        }
+    }
+
+    private static bool IsBgmeMusic(int cueId)
+    {
+        if (cueId >= 2500)
+        {
+            return true;
+        }
+
+        if (cueId >= 678 && cueId <= 835)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private string GetBgmFile(int bgmId)
+        => $"BGME/P4G/{bgmId}.hca";
 }
