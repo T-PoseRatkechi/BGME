325c28c9ddb57f5c0b0f28857acd62ca8a1d97ac
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-08 21:58:23 -0600

    Add battle hook.

---
+++ BGME.Framework/P5R/BattleHooks.cs
@@ -0,0 +1,29 @@
+﻿using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.Definitions.X64;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+
+namespace BGME.Framework.P5R;
+
+internal unsafe class BattleHooks : IGameHook
+{
+    [Function(CallingConventions.Microsoft)]
+    private delegate nint SetParticipantAction(nint* participantPtr, nint param2);
+    private IHook<SetParticipantAction>? setParticipantActionHook;
+
+    public Action<nint, nint>? ParticipantActed;
+
+    public void Initialize(IStartupScanner scanner, IReloadedHooks hooks)
+    {
+        scanner.Scan("SetParticipantAction Function", "48 89 54 24 ?? 55 53 56 57 41 54 41 55 41 56 41 57 48 8D AC 24", result =>
+        {
+            this.setParticipantActionHook = hooks.CreateHook<SetParticipantAction>(this.SetParticipantActionImpl, result).Activate();
+        });
+    }
+
+    private nint SetParticipantActionImpl(nint* participantPtr, nint param2)
+    {
+        Log.Debug($"ParticipantActionSet || {(nint)participantPtr:X} || {param2:X}");
+        this.ParticipantActed?.Invoke((nint)participantPtr, param2);
+        return this.setParticipantActionHook!.OriginalFunction(participantPtr, param2);
+    }
+}
