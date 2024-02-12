bb04c8fbdc79700c5b990424972be6bc7961f015
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-12 14:13:00 -0600

    Add API config and utils.

---
+++ BGME.Framework.API/Utils.cs
@@ -0,0 +1,28 @@
+﻿using Reloaded.Mod.Loader.IO.Utility;
+
+namespace BGME.Framework.API;
+
+internal static class Utils
+{
+    public static FileSystemWatcher CreateWatch(string path, FileSystemEventHandler handler, string? filter = null)
+    {
+        var isFile = File.Exists(path);
+
+        // Use file name as filter for paths that are files.
+        var folder = isFile ? Path.GetDirectoryName(path)! : path;
+        var fileName = isFile ? Path.GetFileName(path) : null;
+
+        var watcher = FileSystemWatcherFactory.Create(
+            folder,
+            handler,
+            null,
+            FileSystemWatcherFactory.FileSystemWatcherEvents.Deleted
+            | FileSystemWatcherFactory.FileSystemWatcherEvents.Created
+            | FileSystemWatcherFactory.FileSystemWatcherEvents.Changed,
+            !isFile,
+            isFile ? fileName : filter,
+            false);
+
+        return watcher;
+    }
+}
--- BGME.Framework/Music/MusicUtils.cs
+++ BGME.Framework/Music/MusicUtils.cs
@@ -87,26 +87,4 @@ internal static class MusicUtils
 
         return CalculateMusicId(music) ?? -1;
     }
-
-    public static FileSystemWatcher CreateWatch(string path, FileSystemEventHandler handler, string? filter = null)
-    {
-        var isFile = File.Exists(path);
-
-        // Use file name as filter for paths that are files.
-        var folder = isFile ? Path.GetDirectoryName(path)! : path;
-        var fileName = isFile ? Path.GetFileName(path) : null;
-
-        var watcher = FileSystemWatcherFactory.Create(
-            folder,
-            handler,
-            null,
-            FileSystemWatcherFactory.FileSystemWatcherEvents.Deleted
-            | FileSystemWatcherFactory.FileSystemWatcherEvents.Created
-            | FileSystemWatcherFactory.FileSystemWatcherEvents.Changed,
-            !isFile,
-            isFile ? fileName : filter,
-            false);
-
-        return watcher;
-    }
 }
