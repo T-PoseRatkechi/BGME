e59dabaf90c259ccbc44fc1bf5b5832d2b04113b
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-13 06:06:05 -0600

    Fix action.

---
+++ PublishAll.ps1
@@ -0,0 +1,14 @@
+# Set Working Directory
+Split-Path $MyInvocation.MyCommand.Path | Push-Location
+[Environment]::CurrentDirectory = $PWD
+
+./Publish.ps1 -ProjectPath "BGME.Framework/BGME.Framework.csproj" `
+              -PackageName "BGME.Framework" `
+              -PublishOutputDir "Publish/ToUpload/Framework" `
+./Publish.ps1 -ProjectPath "BGME.Framework.P3R/BGME.Framework.P3R.csproj" `
+              -PackageName "BGME.Framework.P3R" `
+              -PublishOutputDir "Publish/ToUpload/Framework.P3R" `
+./Publish.ps1 -ProjectPath "BGME.Framework.API/BGME.Framework.API.csproj" `
+              -PackageName "BGME.Framework.API" `
+              -PublishOutputDir "Publish/ToUpload/Framework.API" `
+Pop-Location
