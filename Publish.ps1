845f10b49acad6d8f7d6190c6ffe40ce5a63587f
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-13 05:57:36 -0600

    Fix workflow.

--- Publish.ps1
+++ Publish.ps1
@@ -5,17 +5,14 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
 ./BGME.Framework/Publish.ps1 -ProjectPath "BGME.Framework/BGME.Framework.csproj" `
               -PackageName "BGME.Framework" `
               -PublishOutputDir "Publish/ToUpload/Framework" `
-			  -UseScriptDirectory false `
 			  @args
 			  
 ./BGME.Framework.P3R/Publish.ps1 -ProjectPath "BGME.Framework.P3R/BGME.Framework.P3R.csproj" `
               -PackageName "BGME.Framework.P3R" `
               -PublishOutputDir "Publish/ToUpload/Framework.P3R" `
-			  -UseScriptDirectory false `
 			  @args			  
 ./BGME.Framework.API/Publish.ps1 -ProjectPath "BGME.Framework.API/BGME.Framework.API.csproj" `
               -PackageName "BGME.Framework.API" `
               -PublishOutputDir "Publish/ToUpload/Framework.API" `
-			  -UseScriptDirectory false `
 			  @args
 Pop-Location
