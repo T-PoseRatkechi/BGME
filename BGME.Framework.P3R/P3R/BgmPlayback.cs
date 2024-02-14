56343f9d9c5ac7315812c1c6577cab5da30246f3
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-13 23:08:21 -0600

    Bump version.

--- BGME.Framework.P3R/P3R/BgmPlayback.cs
+++ BGME.Framework.P3R/P3R/BgmPlayback.cs
@@ -10,10 +10,6 @@ namespace BGME.Framework.P3R.P3R;
 internal unsafe class BgmPlayback : BaseSound, IGameHook
 {
     [Function(CallingConventions.Microsoft)]
-    private delegate void PlayBgmFunction(int bgmId);
-    private IHook<PlayBgmFunction>? playBgmHook;
-
-    [Function(CallingConventions.Microsoft)]
     private delegate void PlayCueId(nint param1, int param2, int param3, int cueId);
     private IHook<PlayCueId>? playCueHook;
 
@@ -27,14 +23,6 @@ internal unsafe class BgmPlayback : BaseSound, IGameHook
 
     public void Initialize(IStartupScanner scanner, IReloadedHooks hooks)
     {
-        scanner.Scan(
-            "PlayBgm Function",
-            "89 4C 24 ?? 48 83 EC 48 E8 ?? ?? ?? ?? 48 89 44 24 ?? 48 8B 4C 24 ?? E8 ?? ?? ?? ?? 48 89 44 24 ?? 48 8B 4C 24 ?? E8 ?? ?? ?? ?? 48 89 44 24 ?? 48 83 7C 24 ?? 00 74 ?? 44 8B 4C 24 ?? C6 05 ?? ?? ?? ?? 82",
-            result =>
-        {
-            this.playBgmHook = hooks.CreateHook<PlayBgmFunction>(this.PlayBgm, result);
-        });
-
         scanner.Scan(
             "PlayCueId Function",
             "48 89 5C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 45 31 D2",
@@ -74,23 +62,6 @@ internal unsafe class BgmPlayback : BaseSound, IGameHook
 
     protected override void PlayBgm(int bgmId)
     {
-        var currentBgmId = this.GetGlobalBgmId(bgmId);
-        if (currentBgmId == null)
-        {
-            return;
-        }
-
-        if (bgmId >= 400)
-        {
-            // Manually play.
-            var player = this.criAtomEx.GetPlayerById(0);
-            var strPtr = StringsCache.GetStringPtr($"{bgmId}");
-            this.criAtomEx.Player_SetCueName(player.PlayerHn, 0, (byte*)strPtr);
-            this.criAtomEx.Player_Start(player.PlayerHn);
-        }
-        else
-        {
-            this.playBgmHook!.OriginalFunction(bgmId);
-        }
+        throw new NotImplementedException();
     }
 }
