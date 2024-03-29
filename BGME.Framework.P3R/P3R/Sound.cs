03cb847045c60946ce00fef72a6bb0cdc93496b4
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-03-29 09:08:16 -0500

    P3R: Adjust for new P3R version.

--- BGME.Framework.P3R/P3R/Sound.cs
+++ BGME.Framework.P3R/P3R/Sound.cs
@@ -15,7 +15,7 @@ internal unsafe class Sound : BaseSound, IGameHook
     private PlayBgmFunction? playBgm;
 
     [Function(CallingConventions.Microsoft)]
-    private delegate void RequestSound(UPlayAdxControl* self, int playerMajorId, int playerMinorId, int cueId);
+    private delegate void RequestSound(UPlayAdxControl* self, int playerMajorId, int playerMinorId, int cueId, nint param5);
 
     private readonly ICriAtomEx criAtomEx;
     private IHook<RequestSound>? requestSoundHook;
@@ -30,30 +30,30 @@ internal unsafe class Sound : BaseSound, IGameHook
 
     public void Initialize(IStartupScanner scanner, IReloadedHooks hooks)
     {
-        scanner.Scan(
-            "Play Aria of the Soul Function",
-            "40 53 48 83 EC 20 F3 0F 58 49",
-        result =>
-        {
-            var thunkFunc = *(int*)(result + 0x4a + 1) + result + 0x4a + 5;
-            thunkFunc = *(int*)(thunkFunc + 1) + thunkFunc + 5;
-            var actual = thunkFunc = *(int*)(thunkFunc + 1) + thunkFunc + 5;
-            //this.playBgmHook = hooks.CreateHook<PlayBgmFunction>(this.PlayBgm, actual).Activate();
-            this.playBgm = hooks.CreateWrapper<PlayBgmFunction>(actual, out _);
-        });
+        //scanner.Scan(
+        //    "Play Aria of the Soul Function",
+        //    "40 53 48 83 EC 20 F3 0F 58 49",
+        //result =>
+        //{
+        //    var thunkFunc = *(int*)(result + 0x4a + 1) + result + 0x4a + 5;
+        //    thunkFunc = *(int*)(thunkFunc + 1) + thunkFunc + 5;
+        //    var actual = thunkFunc = *(int*)(thunkFunc + 1) + thunkFunc + 5;
+        //    //this.playBgmHook = hooks.CreateHook<PlayBgmFunction>(this.PlayBgm, actual).Activate();
+        //    this.playBgm = hooks.CreateWrapper<PlayBgmFunction>(actual, out _);
+        //});
 
         scanner.Scan(
             nameof(RequestSound),
-            "48 89 5C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 45 31 D2",
+            "48 89 5C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 4C 89 74 24 ?? 45 31 D2",
             result => this.requestSoundHook = hooks.CreateHook<RequestSound>(this.RequestSoundImpl, result).Activate());
     }
 
-    private void RequestSoundImpl(UPlayAdxControl* self, int playerMajorId, int playerMinorId, int cueId)
+    private void RequestSoundImpl(UPlayAdxControl* self, int playerMajorId, int playerMinorId, int cueId, nint param5)
     {
-        Log.Debug($"{nameof(RequestSound)} || Player: {playerMajorId} / {playerMinorId} || Cue ID: {cueId}");
+        Log.Debug($"{nameof(RequestSound)} || Player: {playerMajorId} / {playerMinorId} || Cue ID: {cueId} || param5: {param5}");
         if (playerMajorId != 0 || playerMinorId != 0)
         {
-            this.requestSoundHook!.OriginalFunction(self, playerMajorId, playerMinorId, cueId);
+            this.requestSoundHook!.OriginalFunction(self, playerMajorId, playerMinorId, cueId, param5);
             return;
         }
 
@@ -74,7 +74,7 @@ internal unsafe class Sound : BaseSound, IGameHook
         }
         else
         {
-            this.requestSoundHook!.OriginalFunction(self, playerMajorId, playerMinorId, (int)currentBgmId);
+            this.requestSoundHook!.OriginalFunction(self, playerMajorId, playerMinorId, (int)currentBgmId, param5);
         }
     }
 
