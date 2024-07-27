a7903520bb2b139978984ddfccafe719a3c5ed64
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-07-27 10:15:18 -0500

    P4G: Add proof of concept for synced transitions between song variations.

--- BGME.Framework/P4G/BgmeService.cs
+++ BGME.Framework/P4G/BgmeService.cs
@@ -14,6 +14,7 @@ internal class BgmeService : IBgmeService, IGameHook
     private readonly EncounterBgm encounterPatcher;
     private readonly FloorBgm floorPatcher;
     private readonly EventBgm eventBgm;
+    private readonly PlaybackService playback;
 
     public BgmeService(ISharedScans scans, ICriAtomRegistry criAtomRegistry, MusicService music)
     {
@@ -23,6 +24,7 @@ internal class BgmeService : IBgmeService, IGameHook
         this.encounterPatcher = new(music);
         this.floorPatcher = new(music);
         this.eventBgm = new(this.sound, music);
+        this.playback = new(scans, criAtomRegistry);
     }
 
     public void Initialize(IStartupScanner scanner, IReloadedHooks hooks)
